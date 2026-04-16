b3def42c685185e6ca66f4a6a175bd4b543ab1ff
yurayurayura0917-del <yurayurayura0917@gmail.com>
2026-04-16 20:26:13 +0900

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ for m in st.session_state.memories:
 # =====================
 st.title("📍 おいしいお店保存するアプリ")
 
-col1, col2 = st.columns(3)
+col1, col2, col3 = st.columns([1, 1, 6])
 
 with col1:
     if st.button("📚 一覧"):
@@ -80,11 +80,6 @@ with col2:
         st.session_state.page = "map"
         st.rerun()
 
-if "liked" not in st.session_state:
-    st.session_state.liked = set()
-
-col1, col2, col3 = st.columns([1, 8, 1])
-
 with col3:
     if st.button("＋"):
         st.session_state.page = "add"
@@ -93,6 +88,7 @@ with col3:
 # =====================
 # 入力
 # =====================
+elif st.session_state.page == "add":
     st.subheader("📍 思い出を追加")
 
     query = st.text_input("場所")
